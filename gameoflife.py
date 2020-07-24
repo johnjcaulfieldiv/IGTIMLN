ecd2d719fbec1694830a5fa5301922eb6f11320a
johnjcaulfieldiv <johnjcaulfieldiv@gmail.com>
2020-07-24 12:00:25 -0700

    save neighbor indices in cache instead of calculating every update

--- gameoflife.py
+++ gameoflife.py
@@ -1,4 +1,6 @@
-""" CONWAY'S GAME OF LIFE - RULES
+"""
+
+CONWAY'S GAME OF LIFE - RULES
 
 Any live cell with two or three live neighbours survives.
 Any dead cell with three live neighbours becomes a live cell.
@@ -9,7 +11,7 @@ All other live cells die in the next generation. Similarly, all other dead cells
 # TODO
 # save/load using file
 # command line options
-# run n iterations
+# DONE run n iterations
 # DONE add history to go forward/back generations
 # try cacheing all neighbors for each cell to speed up update()ing
 
@@ -24,15 +26,43 @@ class Life():
         self.cells = ['*'] * self.height * self.width
         self.history = []
         self.history_index = -1
-        self.forward = []
+        self.neighbors = []
+        self.setup_neighbors()
+
+    def sizeof_neighbors(self):
+        count = 0
+        for group in self.neighbors:
+            for item in group:
+                count += 1
+        return count
 
     def update(self):
         "sets each cell as alive or dead following game rules"
-        self.cells = ['*' if self.lives(cell, self.get_neighbor_counts(index)) else ' ' for index, cell in enumerate(self.cells)]
+        self.cells = ['*' if self.lives(cell, index) else ' ' for index, cell in enumerate(self.cells)]
         if self.history_index != len(self.history)-1:
             self.history = self.history[:self.history_index+1]
         self.history.append(self.cells)
         self.history_index += 1
+    
+    def lives(self, cell, index):
+        """ Returns True if cell is living or False if cell is dead based on living neighbors """
+        living_neighbors = 0
+        for n in self.neighbors[index]:
+            if self.cells[n] == '*':
+                living_neighbors += 1
+        return (cell == '*' and (living_neighbors == 2 or living_neighbors == 3) or
+                cell == ' ' and living_neighbors == 3)
+
+    def setup_neighbors(self):
+        for index in range(len(self.cells)):
+            self.neighbors.append([])
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    if x == 0 and y == 0:
+                        continue
+                    row = ((index + self.width * x) % len(self.cells)) // self.width
+                    neighbor = (index + y) % self.width + row * self.width
+                    self.neighbors[index].append(neighbor)
 
     def get_neighbor_counts(self, index):
         alive = 0
@@ -79,11 +109,6 @@ class Life():
         self.history_index = len(self.history)-1
         self.cells = self.history[self.history_index]
 
-    def lives(self, cell, living_neighbors):
-        """ Returns True if cell is living or False if cell is dead based on living neighbors """
-        return (cell == '*' and (living_neighbors[0] == 2 or living_neighbors[0] == 3) or
-            cell == ' ' and living_neighbors[0] == 3)
-
     def randomize(self, perc):
         "Sets perc % of the cells in the grid as alive (roughly)"
         self.cells = list(map(lambda c: '*' if random.randint(1,100) <= perc else ' ', self.cells))
@@ -106,7 +131,8 @@ class Life():
     def print_all(self):
         all =  "height:     {}\n".format(self.height)
         all += "width:      {}\n".format(self.width)
-        all += "len(cells): {}".format(len(self.cells))
+        all += "len(cells): {}\n".format(len(self.cells))
+        all += "len(cache): {}".format(self.sizeof_neighbors())
         print(all)
 
     def start(self):
@@ -156,6 +182,8 @@ class Life():
             if response.startswith('e'):
                 self.forward_to_last()
                 self.REPL_print_grid()
+            if response.startswith('p'): # print info
+                self.print_all()
             if response.startswith('a'): # run it back! [a]gain!
                 main()
                 return
@@ -187,10 +215,10 @@ class Life():
         # run the game without any ui until a loop is found or n gens pass
         self.randomize(perc)
         while (n > 1 and not self.is_looping()):
-            n -= 1
-            os.system('cls')
-            print()
-            print("Running out the game. Gen " + str(self.history_index+1))
+            #n -= 1
+            #os.system('cls')
+            #print()
+            #print("Running out the game. Gen " + str(self.history_index+1))
             self.update()
         self.REPL_print_grid()
         # start a REPL to view game states
@@ -201,7 +229,7 @@ def main():
     #Life(55,80).auto_detect_loop(80, .1) # large, good for ogling
     #Life(5,5).auto_detect_loop(60, .1)  # small, good for testing
     #Life(55,80).auto_finish(50)
-    Life(55,80).auto_n_generations(50, 50)
+    Life(60,60).auto_n_generations(5000, 50)
 
 if __name__ == "__main__":
     main()
